ebd3642433274f266291e19e730af91fd9063c52
ZiaUrRehman-bit <engrziaurrehman.kicsit@gmail.com>
2024-01-30 11:51:32 +0500

    update

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import cv2
 import time
+from ultralytics import YOLO
+from tracker import*
+
+model = YOLO('yolov8s.pt')
+
+# read the classes from coco.txt file
+myFile = open("coco.txt",'r')
+data = myFile.read()
+classList = data.split("\n")
+print(classList)
 
 cam = cv2.VideoCapture("vidp.mp4")
 
 cTime = 0
 pTime = 0
 
+count = 0
+personDown = {}
+tracker = Tracker()
+counter1 = []
+
+personUp = {}
+counter2 = []
+
+cy1 = 194
+cy2 = 220
+offset = 6
+
 while True:
     Success, frame = cam.read()
+    if not Success:
+        break
+
+    
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
